717adab4e9544d04cb5af05724c277b8b6b1ae8a
CyberMedina <jhonatanmedina5255@gmail.com>
2025-05-15 01:48:27 -0600

    test: Agregar CrearCuenta

---
+++ prueba-tecnica/Tests/CuentaServicesTests.cs
@@ -0,0 +1,22 @@
+﻿using prueba_tecnica.Models;
+using prueba_tecnica.Services;
+using Xunit;
+
+
+namespace prueba_tecnica.Tests
+{
+    public class CuentaServicesTests
+    {
+        private readonly CuentaService _service = new();
+
+        [Fact]
+        public void CrearCuenta_ConSaldoInicial_DeberiCrearCuentaCorrectamente()
+        {
+            var cuenta = _service.CrearCuenta(1000, out string error);
+
+            Assert.NotNull(cuenta);
+            Assert.Equal(1000, cuenta.Saldo);
+            Assert.Equal(string.Empty, error);
+        }
+    }
+}
