243415e9450cf776bf5f76014a1752c89a1c88ed
CyberMedina <jhonatanmedina5255@gmail.com>
2025-05-14 22:44:03 -0600

    feat(Models): agregar clases CuentaBancaria y Transaccion con sus propiedades

---
+++ prueba-tecnica/Models/CuentaBancaria.cs
@@ -0,0 +1,9 @@
+﻿namespace prueba_tecnica.Models
+{
+    public class CuentaBancaria
+    {
+        public string NumeroCuenta { get; set; } = string.Empty;
+        public decimal Saldo { get; set; }
+        public List<Transaccion> Transacciones { get; set; } = new ();
+    }
+}
---
+++ prueba-tecnica/Models/Transaccion.cs
@@ -0,0 +1,11 @@
+﻿namespace prueba_tecnica.Models
+{
+    public class Transaccion
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Tipo { get; set; } = string.Empty;
+        public decimal Monto { get; set; }
+        public decimal SaldoDespues { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.UtcNow;
+    }
+}
